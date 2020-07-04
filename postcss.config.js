065f360f072c4db1245a1672c4e6a2a5f1e17bcb
Tim Fleming <tfleming26@hotmail.com>
2020-07-03 18:18:47 -0600

    Update config files

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
@@ -26,3 +26,5 @@ if (process.env.RAILS_ENV === 'production') {
     })
   )
 }
+
+module.exports = environment;
