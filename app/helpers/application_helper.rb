d709ae8ec4b812045fd902d6cbf727dc93e317e3
Tim Fleming <tfleming26@hotmail.com>
2020-07-05 13:41:11 -0600

    Add title for recipe show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def title(*items)
+    unless items.empty?
+      content_for :title do
+        (items << "Real Prairie Delights").join(" - ")
+      end      
+    end
+  end
 end
--- spec/features/creating_recipes_spec.rb
+++ spec/features/creating_recipes_spec.rb
@@ -15,5 +15,11 @@ RSpec.feature 'Users can create recipes' do
     click_button 'Create Recipe'
 
     expect(page).to have_content 'Your recipe has been created.'
+
+    recipe = Recipe.find_by(recipe_name: "Pancakes")
+    expect(page.current_url).to eq recipe_url(recipe)
+
+    title = "Pancakes - Recipes - Real Prairie Delights"
+    expect(page).to have_title title
   end
 end
